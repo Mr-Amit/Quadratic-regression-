c2f9131fed242ac347a84e7b5948a6003e6500e3
Amit Kumar <44119580+Mr-Amit@users.noreply.github.com>
2021-01-03 01:14:52 +0530

    Minor changes

--- quadratic.java
+++ quadratic.java
@@ -1,19 +1,10 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.lang.Math;
 import org.apache.commons.math3.fitting.PolynomialCurveFitter;
-import org.apache.commons.math4.fitting.WeightedObservedPoints;
-//import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.apache.commons.math3.fitting.WeightedObservedPoints;
 import java.util.*;
 import java.io.*; 
-
-public class MainClass {
-    public static void main(String args[]) {
-        Coeffs a = new Coeffs();
-        
-    }
-}
 
 class XnY{
     double x, y;
@@ -31,21 +22,20 @@ class XnY{
     }
 }
 class Coeffs{
-    // declare 2 one dimensional array
-    // 1 for storing the value pairs
-    // 1 for storing the calculations till then
+
     ArrayList<XnY> xny; 
     double[][] arr; 
     int uptill;
     WeightedObservedPoints obs;
     int prev;
     double[] prev_coeff;
+    int done;
 
     Coeffs(){
-
         uptill = -1;
         obs = new WeightedObservedPoints();
         prev = 0;
+        done = 0;
         xny =new ArrayList<XnY>();
         arr = new double[5][5];
         prev_coeff = new double[3];
@@ -53,7 +43,12 @@ class Coeffs{
     
     public double[][] getCoeffs(double x, double y, double sensitivity, int sig_dig){
         PolynomialCurveFitter fitter = PolynomialCurveFitter.create(2);
+        if(done != 0){
+            prev_coeff = fitter.fit(obs.toList());
+        }
+        
         obs.add(x,y);
+        done = 1;
         xny.add(new XnY(x, y));
         double[] coeffs = fitter.fit(obs.toList());
         //System.out.println(coeffs[0] + ", " + coeffs[1] + ", " + coeffs[2]);
@@ -62,18 +57,19 @@ class Coeffs{
             return arr;
         }
         else{
+            update(prev_coeff);
             obs.clear();
-            prev = (xny.get(xny.size() -1)).getv(0);
+            prev = xny.size() -1;
             xny.clear();
             obs.add(x, y);
             xny.add(new XnY(x, y));
             coeffs = defaultCoeff();
             update(coeffs);
+            return arr;
         }
-        
-        
-                
+            
     }
+
 
     double[] defaultCoeff(){
         double[] def = {-0.01, -0.01, 1};
@@ -83,7 +79,6 @@ class Coeffs{
     
     double mean(){
         double sum = 0;
-
         for(XnY ele: xny){
             sum += ele.getv(1);
         }
@@ -105,18 +100,16 @@ class Coeffs{
 
     double sensitivityCalc(double[] coeffs){
 
-
         double[] predictedValues = new double[xny.size()];
         double residualSumOfSquares = 0;
         
         for (int i=prev; i< xny.size(); i++) {
             predictedValues[i] = predict(coeffs, xny.get(i).getv(0));
-
             double Val = xny.get(i).getv(1);
             double t = Math.pow((predictedValues[i] - Val), 2);
             residualSumOfSquares  += t;
-            
         }
+        
         double avgActualValue = mean();
         double totalSumOfSquares = 0;
         for (int i=prev; i<xny.size(); i++) {
@@ -126,4 +119,13 @@ class Coeffs{
         return 1.0 - (residualSumOfSquares/totalSumOfSquares);
     }
     
+}
+
+
+
+public class App {
+    public static void main(String args[]) {
+        Coeffs a = new Coeffs();
+        
+    }
 }
