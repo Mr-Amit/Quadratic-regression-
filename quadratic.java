0b10faec54ff9fb5cbd0e918e876862f00a1b487
Amit Kumar <44119580+Mr-Amit@users.noreply.github.com>
2021-01-03 15:09:04 +0530

    Major commit
    Has my additional I code used for testing

--- quadratic.java
+++ quadratic.java
@@ -1,7 +1,83 @@
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
 import java.lang.Math;
+import java.util.Scanner; 
+import java.io.*;
 import org.apache.commons.math3.fitting.PolynomialCurveFitter;
 import org.apache.commons.math3.fitting.WeightedObservedPoints;
+
+
+public class quadratic {
+    public static void main(String args[]) throws Exception {
+        Coeffs a = new Coeffs();
+        double[][] ans = new double[5][10];
+        double[] xvals = new double[1200];
+        double[] yvals = new double[1200];
+
+        File file = new File("C:\\Users\\lenovo\\Desktop\\JAVA\\textFile.txt"); //You dont need to do anything here I made it for python
+          
+        file.delete();
+        FileWriter wrt = new FileWriter("C:\\Users\\lenovo\\Desktop\\JAVA\\textFile.txt"); //You dont need to do anything here I made it for python
+
+        try {
+     
+            // Put the input.txt in the desired location 
+            File myObj = new File("C:\\Users\\lenovo\\Downloads\\input.txt");
+            // This input file contains all the data value pairs from the Data.csv file
+            Scanner myReader = new Scanner(myObj);
+            int i = 0;
+            while (myReader.hasNextLine()) {
+                String str = myReader.nextLine();
+                String[] strs = str.split("\t");
+                xvals[i] = Double.parseDouble(strs[0]);
+                yvals[i] = Double.parseDouble(strs[1]);
+                i += 1;
+            }
+            myReader.close();
+            } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        w2f(xvals, wrt, 10);
+        w2f(yvals, wrt, 10);
+
+        for(int i = 0; i < 300; i ++){
+            ans = a.getCoeffs(xvals[i], yvals[i], 2, 6, 2);                 //Function call to get Coefficients
+            print2D(ans,  wrt);
+        }
+
+        wrt.close();
+
+    }
+    static void print2D(double mat[][],  FileWriter wrt) throws Exception{ 
+        wrt.write("\n");
+
+        for (int i = 0; i < mat.length; i++){
+  
+            System.out.print("[");
+            for (int j = 0; j < 5; j++) 
+                if(mat[i][0] != 0 || mat[i][1] !=0)
+                    if(j < 4)
+                        System.out.print(mat[i][j] + ", "); 
+                    else
+                        System.out.print(mat[i][j]); 
+            System.out.println("]");
+            w2f(mat[i], wrt, 5);
+            
+        }
+        System.out.println("\n");
+    }
+
+    static void w2f(double[] arr, FileWriter wrt, int len) throws Exception{
+        //int len = arr.length;
+        for (int i = 0; i < len; i++) {
+           wrt.write(arr[i] + " ");
+        }
+        wrt.write("\n");
+    }
+
+}
 
 
 class XnY{
@@ -19,6 +95,7 @@ class XnY{
         }
     }
 }
+
 class Coeffs{
 
     ArrayList<XnY> xny; 
@@ -28,100 +105,108 @@ class Coeffs{
     int prev;
     double[] prev_coeff;
     int done;
-
     Coeffs(){
         uptill = 0;
         obs = new WeightedObservedPoints();
         prev = 0;
         done = 0;
         xny =new ArrayList<XnY>();
-        arr = new double[5][10];                //Changes
+        arr = new double[10][5];                
         prev_coeff = new double[3];
+        
     }
     
 
+
     // The Required Function
-    public double[][] getCoeffs(double x, double y, double sensitivity, int sig_dig){
+    public double[][] getCoeffs(double x, double y, double tolerance, int minFailedPred, int sig_dig){
         PolynomialCurveFitter fitter = PolynomialCurveFitter.create(2);
         if(done != 0){
             prev_coeff = fitter.fit(obs.toList());
         }
         done = 1;
-
+        x = Math.round (x * 10000.0) / 10000.0;
+        y = Math.round (y * 10000.0) / 10000.0;
         obs.add(x,y);
         done = 1;
         xny.add(new XnY(x, y));
-        double[] coeffs = fitter.fit(obs.toList());
-        //System.out.println(coeffs[0] + ", " + coeffs[1] + ", " + coeffs[2]);
-        double calcSensitivity = sensitivityCalc(coeffs);
-        System.out.println("Sensitivity : " + calcSensitivity);
-        if(sensitivity < calcSensitivity){
+        double[] coeffs = fitter.fit(obs.toList()); 
+        if(xny.size() < 6){
+            // My implementation of your virtual point
+            if(coeffs[2] > 0 && xny.size() > 2){
+                double xprev = x; 
+                double avgDist = (x- xny.get(xny.size() - 2).getv(0))/2;
+                double yprev = y; 
+                x = xprev + avgDist;
+                y = yprev/2;
+                x = Math.round (x * 10000.0) / 10000.0;
+                y = Math.round (y * 10000.0) / 10000.0;
+                obs.add(x, y);
+                xny.add(new XnY(x, y));
+            }
+            coeffs = fitter.fit(obs.toList());
             update(coeffs, sig_dig);
             return arr;
         }
-        else{
-            update(prev_coeff, sig_dig);
-            uptill += 1;
-            obs.clear();
-            prev = xny.size() -1;
-            xny.clear();
-            obs.add(x, y);
-            xny.add(new XnY(x, y));
-            // coeffs = defaultCoeff();
-            // update(coeffs, 1);
-            return arr;
+        else{  
+            int FailedPred = FailiureCalc(coeffs, tolerance);
+            if(minFailedPred > FailedPred){
+                if(coeffs[2] > 0 && xny.size() > 2){    //Virtual point 
+                    double xprev = x; 
+                    double avgDist = (x- xny.get(xny.size() - 2).getv(0))/2;
+                    double yprev = y; 
+                    x = xprev + avgDist;
+                    y = yprev/2;
+                    x = Math.round (x * 10000.0) / 10000.0;
+                    y = Math.round (y * 10000.0) / 10000.0;
+                    obs.add(x, y);
+                    xny.add(new XnY(x, y));
+                }
+                coeffs = fitter.fit(obs.toList());
+                update(coeffs, sig_dig);
+                return arr;
+            }
+            else{
+                update(prev_coeff, sig_dig);
+                uptill += 1;
+                obs.clear();
+                xny.clear();
+                obs.add(x, y);
+                xny.add(new XnY(x, y));
+                return arr;
+            }
         }
     }
 
 
-    double[] defaultCoeff(){
-        double[] def = {-0.01, 1, -0.01};
-        return def;
-    }
-
-    
-    double mean(){
-        double sum = 0;
-        for(XnY ele: xny){
-            sum += ele.getv(1);
-        }
-        System.out.println("Mean " + sum/xny.size());
-        return sum/xny.size();
-    }
-
-    void update(double[] coeffs, int i){
-        uptill = (uptill)%5;
+    void update(double[] coeffs, int sig_dig){
+        uptill = (uptill)%10;
         arr[uptill][0] = xny.get(0).getv(0);
+        System.out.println("The size : " + xny.size());                             // print length
         arr[uptill][1] = xny.get(xny.size() -1).getv(0);
-        arr[uptill][2] = Math.round(coeffs[2]*Math.pow(10,i))/Math.pow(10,i);
-        arr[uptill][3] = Math.round(coeffs[1]*Math.pow(10,i))/Math.pow(10,i);
-        arr[uptill][4] = Math.round(coeffs[0]*Math.pow(10,i))/Math.pow(10,i);
+        arr[uptill][2] = Math.round(coeffs[2]*Math.pow(10,sig_dig))/Math.pow(10,sig_dig);
+        arr[uptill][3] = Math.round(coeffs[1]*Math.pow(10,sig_dig))/Math.pow(10,sig_dig);
+        arr[uptill][4] = Math.round(coeffs[0]*Math.pow(10,sig_dig))/Math.pow(10,sig_dig);
     }
 
     double predict(double [] coeffs, double x){
         return coeffs[2]*x*x + coeffs[1]*x + coeffs[1];
     }
 
-    double sensitivityCalc(double[] coeffs){
+    int FailiureCalc(double[] coeffs, double tolerance){
 
-        double[] predictedValues = new double[xny.size()];
-        double residualSumOfSquares = 0;
+        double calcY;
+        int FailedPred = 0;
         
         for (int i=0; i< xny.size(); i++) {
             
-            predictedValues[i] = predict(coeffs, xny.get(i).getv(0));
+            calcY = predict(coeffs, xny.get(i).getv(0));
             double Val = xny.get(i).getv(1);
-            double t = Math.pow((predictedValues[i] - Val), 2);
-            residualSumOfSquares  += t;
+            if(Val > calcY + tolerance){
+                FailedPred += 1;
+            }
         }
-
-        double avgActualValue = mean();
-        double totalSumOfSquares = 0;
-        for (int i=0; i<xny.size(); i++) {
-            totalSumOfSquares += Math.pow( (predictedValues[i] - avgActualValue),2);
-
-        }
-        return 1.0 - (residualSumOfSquares/totalSumOfSquares);
+        return FailedPred;
     }
     
 }
